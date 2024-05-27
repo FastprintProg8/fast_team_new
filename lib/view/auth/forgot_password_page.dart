8fd97160deb8e0fad4ce2ad5b3a2595210aeb422
brian2zz <84264187+brian2zz@users.noreply.github.com>
2024-05-27 14:59:07 +0700

    "Refactored ForgotPasswordPage.dart: removed unnecessary whitespace, reformatted code, and made minor adjustments to indentation and syntax."

--- lib/view/auth/forgot_password_page.dart
+++ lib/view/auth/forgot_password_page.dart
@@ -10,9 +10,8 @@ class ForgotPasswordPage extends StatefulWidget {
 }
 
 class _ForgotPasswordPage extends State<ForgotPasswordPage> {
-
   bool isLoading = false;
-  
+
   LoginController? loginController;
   TextEditingController? emailInput;
 
@@ -22,22 +21,22 @@ class _ForgotPasswordPage extends State<ForgotPasswordPage> {
     emailInput = TextEditingController();
     loginController = Get.put(LoginController());
   }
-  
+
   showSnackBar(String message, Color color) {
     snackbar() => SnackBar(
-      content: Text(
-        message,
-        style: const TextStyle(
-            fontSize: 12.0,
-            fontWeight: FontWeight.w500,
-            color: Colors.white,
-        ),
-      ),
-      backgroundColor: color,
-      duration: const Duration(milliseconds: 2000),
-    );
+          content: Text(
+            message,
+            style: const TextStyle(
+              fontSize: 12.0,
+              fontWeight: FontWeight.w500,
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: color,
+          duration: const Duration(milliseconds: 2000),
+        );
     ScaffoldMessenger.of(context).showSnackBar(snackbar());
-  } 
+  }
 
   void showCustomDialog(BuildContext context, String message) {
     showDialog(
@@ -63,70 +62,69 @@ class _ForgotPasswordPage extends State<ForgotPasswordPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          'Reset Password',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
+          title: const Text(
+            'Reset Password',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        ),
-        centerTitle: true, 
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            // Custom back button action
-            Navigator.pop(context, 'true');
-          },
-        )
-      ),
+          centerTitle: true,
+          automaticallyImplyLeading: false,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back),
+            onPressed: () {
+              // Custom back button action
+              Navigator.pop(context, 'true');
+            },
+          )),
       body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              const SizedBox(height: 70.0),
-              _logo(),
-              const SizedBox(height: 40.0),
-              _infoMessage(),
-              const SizedBox(height: 40.0),
-              _emailField(),
-              const SizedBox(height: 16.0),
-              _buildButton(context, emailInput!, loginController!)
-            ]
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  const SizedBox(height: 70.0),
+                  _logo(),
+                  const SizedBox(height: 40.0),
+                  _infoMessage(),
+                  const SizedBox(height: 40.0),
+                  _emailField(),
+                  const SizedBox(height: 16.0),
+                  _buildButton(context, emailInput!, loginController!)
+                ]),
           ),
         ),
       ),
     );
   }
 
-  ElevatedButton _buildButton(BuildContext context, 
-      TextEditingController emailInput,
-      LoginController controller) {
-    
+  ElevatedButton _buildButton(BuildContext context,
+      TextEditingController emailInput, LoginController controller) {
     requestResetPassword() async {
-        var result = await controller.requestResetPassword(emailInput.text);
-        print(result);
-        if (result['status'] == 200) {
-          showCustomDialog(context, 'Password reset instructions sent to your email.');
-        } else if (result['status'] == 400){
-          String message = '';
+      var result = await controller.requestResetPassword(emailInput.text);
+      print(result);
+      if (result['status'] == 200) {
+        showCustomDialog(
+            context, 'Password reset instructions sent to your email.');
+      } else if (result['status'] == 400) {
+        String message = '';
 
-          if (result['details']['email'] is List) {
-            message = result['details']['email'][0];
-          } else {
-            message = result['details']['email'];
-          }
-          
-          showSnackBar(message, Colors.red);
+        if (result['details']['email'] is List) {
+          message = result['details']['email'][0];
+        } else {
+          message = result['details']['email'];
         }
+
+        showSnackBar(message, Colors.red);
+      }
     }
 
     validateFromInput() async {
-      if(emailInput.text.isEmpty) {
+      if (emailInput.text.isEmpty) {
         showSnackBar("Email cannot be empty", Colors.red);
       } else {
         await requestResetPassword();
@@ -138,32 +136,35 @@ class _ForgotPasswordPage extends State<ForgotPasswordPage> {
 
     return ElevatedButton(
       onPressed: () async {
-        if(!isLoading) {
+        if (!isLoading) {
           setState(() {
             isLoading = true;
           });
 
           validateFromInput();
         }
-      }, 
+      },
       style: ElevatedButton.styleFrom(
         backgroundColor: Colors.blue[900],
-        splashFactory: InkSplash.splashFactory, 
+        splashFactory: InkSplash.splashFactory,
         minimumSize: const Size(double.infinity, 48.0),
-      ).copyWith(
-        overlayColor: MaterialStateProperty.resolveWith<Color>(
-          (Set<MaterialState> states) {
-            if (states.contains(MaterialState.pressed)) {
-              // Customize the overlay color when pressed
-              return Colors.blue[800]!;
-            }
-            return Colors.transparent;
-          },
-        )
-      ),
-      child: isLoading ? 
-        const CircularProgressIndicator(color: Colors.white,) 
-        : const Text('Submit', style: TextStyle(color: Colors.white),),
+      ).copyWith(overlayColor: MaterialStateProperty.resolveWith<Color>(
+        (Set<MaterialState> states) {
+          if (states.contains(MaterialState.pressed)) {
+            // Customize the overlay color when pressed
+            return Colors.blue[800]!;
+          }
+          return Colors.transparent;
+        },
+      )),
+      child: isLoading
+          ? const CircularProgressIndicator(
+              color: Colors.white,
+            )
+          : const Text(
+              'Submit',
+              style: TextStyle(color: Colors.white),
+            ),
     );
   }
 
