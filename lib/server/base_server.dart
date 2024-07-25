9e4fc8caee45735ec99f86bfb4c0b29dc1571281
brian2zz <84264187+brian2zz@users.noreply.github.com>
2024-07-25 08:29:56 +0700

    "Updated serverUrl in BaseServer class from http://103.29.214.154:9002 to http://103.29.214.154:8002 and commented out the old value."

--- lib/server/base_server.dart
+++ lib/server/base_server.dart
@@ -1,6 +1,7 @@
 class BaseServer {
   // static var serverUrl = "http://13.229.210.140:9002";
-  static var serverUrl = "http://103.29.214.154:9002";
+  // static var serverUrl = "http://103.29.214.154:9002";
+  static var serverUrl = "http://103.29.214.154:8002";
 
   static var durationlimit = const Duration(seconds: 30); 
 }
